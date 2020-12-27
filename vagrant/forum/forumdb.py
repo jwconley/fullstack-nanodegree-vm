d1bfcf9a8037bd2894a615a83ed563d2d8590e4e
John Conley <conley.johnathan@gmail.com>
2020-12-27 18:28:59 -0500

    Corrected file conflict

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -3,23 +3,23 @@
 import datetime
 import psycopg2
 
-# POSTS = [("This is the first post.", datetime.datetime.now())]
+DBNAME = "forum"
 
 def get_posts():
   """Return all posts from the database, most recent first."""
-  db = psycopg2.connect("dbname=forum")
+  db = psycopg2.connect(database=DBNAME)
   cursor = db.cursor()
   get_post_query = "select content, time from posts order by time desc;"
   cursor.execute(get_post_query)
-  results = cursor.fetchall
+  posts = cursor.fetchall
   db.close()
-  return results
+  return posts
 
 def add_post(content):
   """Add a post to the database with the current timestamp."""
-  db = psycopg2.connect("dbname=forum")
+  db = psycopg2.connect(database=DBNAME)
   cursor = db.cursor()
-  add_post_query = "insert into posts values (" + content + ");"
+  add_post_query = "insert into posts values ('%s');" % content
   cursor.execute(add_post_query)
   db.commit()
   db.close()
